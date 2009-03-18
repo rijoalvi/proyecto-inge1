93db1828ef1d6b8511ce80683ea89d34a0bff3db
tarassenkor <tarassenkor@40e6c418-12a5-11de-a1e7-776c1f3a7222>
2009-03-18 16:46:30 +0000

    Sirve

--- Gestion Tipo Campo/src/gestiontipocampo/GestionTipoCampoView.java
+++ Gestion Tipo Campo/src/gestiontipocampo/GestionTipoCampoView.java
@@ -149,6 +149,7 @@ public class GestionTipoCampoView extends FrameView {
         fileMenu.add(jMenuItem1);
 
         exitMenuItem.setAction(actionMap.get("quit")); // NOI18N
+        exitMenuItem.setText(resourceMap.getString("exitMenuItem.text")); // NOI18N
         exitMenuItem.setName("exitMenuItem"); // NOI18N
         fileMenu.add(exitMenuItem);
 
